4401f45d50f2d0360624f1c6ffb11a795d0dd435
Rajnish <rajnish.kumar@impactanalytics.co>
2024-12-02 07:39:58 +0530

    added add task button and search functionality

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -4,13 +4,64 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import logo from "/todo.svg?url";
+import { styled, alpha } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
+import { Box } from "@mui/material";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  width: "100%",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    [theme.breakpoints.up("sm")]: {
+      width: "16ch",
+      "&:focus": {
+        width: "25ch",
+      },
+    },
+  },
+}));
 
 function ResponsiveAppBar() {
   return (
     <AppBar position="sticky">
       <Container maxWidth="xl">
         <Toolbar
-          sx={{ alignContent: "center", justifyContent: "center" }}
+          sx={{
+            display: "flex",
+            alignContent: "center",
+          }}
           disableGutters
         >
           <img
@@ -22,7 +73,6 @@ function ResponsiveAppBar() {
             variant="h6"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
             sx={{
               mr: 2,
               fontFamily: "monospace",
@@ -34,6 +84,23 @@ function ResponsiveAppBar() {
           >
             TickIt
           </Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+          <Button
+            sx={{ color: "#FFF" }}
+            variant="outlined"
+            startIcon={<AddIcon />}
+          >
+            Add Task
+          </Button>
         </Toolbar>
       </Container>
     </AppBar>
